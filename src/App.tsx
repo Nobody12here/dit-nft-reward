c68cb475cb0738b69723ab8f6a3f72b35079b2cb
Nobody12here <uraja01313@gmail.com>
2025-06-03 21:10:59 +0500

    Fixed some issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import { ConfigProvider, theme } from 'antd';
-import { Home } from './pages/home/index';
-import { Web3Provider } from './config';
+import { ConfigProvider, theme } from "antd";
+import { Home } from "./pages/home/index";
+import { Web3Provider } from "./config";
 import { ToastContainer } from "react-toastify";
 
 function App() {
-
   return (
     <Web3Provider>
       <ConfigProvider
         theme={{
           algorithm: theme.darkAlgorithm,
           token: {
-            colorPrimary: '#3b82f6',
+            colorPrimary: "#3b82f6",
             borderRadius: 12,
           },
         }}
       >
-        <ToastContainer>
+        <ToastContainer />
 
-          <Home />
-        </ToastContainer>
+        <Home />
       </ConfigProvider>
     </Web3Provider>
-
   );
 }
 
